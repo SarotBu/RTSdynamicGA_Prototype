08f830cb4eaf44e089d5cc0a60f3e0c487bbc645
SaBuZa <sabuza97@gmail.com>
2017-10-23 09:22:48 +0700

    Change Gene to Rule

---
+++ RTSdynamicGA_Prototype/DynamicScripting.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+#include "GA_SC2.cpp"
+
+#define RULES_LIMIT 100
+
+vector <int,script> scriptList; // scriptList[State] -> Script1 -> Script2 -> ...
+
+
+
+int main(){
+
+    return 0;
+}
