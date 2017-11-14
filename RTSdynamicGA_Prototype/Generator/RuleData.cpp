8bccbed1da32d6f23d13100ede06847e5dd00330
SaBuZa <sabuza97@gmail.com>
2017-11-14 16:36:48 +0700

    update all prototype rules except resource rules

--- RTSdynamicGA_Prototype/Generator/RuleData.cpp
+++ RTSdynamicGA_Prototype/Generator/RuleData.cpp
@@ -11,7 +11,6 @@
     Units barracks_tech = observation->GetUnits(Unit::Self, IsUnit(UNIT_TYPEID::TERRAN_BARRACKSTECHLAB));
     Units factorys_tech = observation->GetUnits(Unit::Self, IsUnit(UNIT_TYPEID::TERRAN_FACTORYTECHLAB));
     Units starports_tech = observation->GetUnits(Unit::Self, IsUnit(UNIT_TYPEID::TERRAN_STARPORTTECHLAB));
-
     Units supply_depots = observation->GetUnits(Unit::Self, IsUnit(UNIT_TYPEID::TERRAN_SUPPLYDEPOT));
 */
 void buildORBITALCOMMAND(){
@@ -42,7 +41,7 @@ void buildBARRACKADDON(){
     }
 }
 
-void BUILDFACTORY(){
+void buildFACTORY(){
     for (const auto& factory : factorys) {
         if (!factory->orders.empty()) {
             continue;
@@ -98,12 +97,77 @@ void buildSTARPORT(){
 }
 
 //RESEARCH RULE
+/*
+if (CountUnitType(observation, UNIT_TYPEID::TERRAN_ARMORY) > 0) {
+                    if (upgrade == UPGRADE_ID::TERRANINFANTRYWEAPONSLEVEL1 && base_count > 2) {
+                        TryBuildUnit(ABILITY_ID::RESEARCH_TERRANINFANTRYWEAPONS, UNIT_TYPEID::TERRAN_ENGINEERINGBAY);
+                    }
+                    else if (upgrade == UPGRADE_ID::TERRANINFANTRYARMORSLEVEL1 && base_count > 2) {
+                        TryBuildUnit(ABILITY_ID::RESEARCH_TERRANINFANTRYARMOR, UNIT_TYPEID::TERRAN_ENGINEERINGBAY);
+                    }
+                    if (upgrade == UPGRADE_ID::TERRANINFANTRYWEAPONSLEVEL2 && base_count > 4) {
+                        TryBuildUnit(ABILITY_ID::RESEARCH_TERRANINFANTRYWEAPONS, UNIT_TYPEID::TERRAN_ENGINEERINGBAY);
+                    }
+                    else if (upgrade == UPGRADE_ID::TERRANINFANTRYARMORSLEVEL2 && base_count > 4) {
+                        TryBuildUnit(ABILITY_ID::RESEARCH_TERRANINFANTRYARMOR, UNIT_TYPEID::TERRAN_ENGINEERINGBAY);
+                    }
+                }
+                TryBuildUnit(ABILITY_ID::RESEARCH_STIMPACK, UNIT_TYPEID::TERRAN_BARRACKSTECHLAB);
+                TryBuildUnit(ABILITY_ID::RESEARCH_TERRANINFANTRYWEAPONS, UNIT_TYPEID::TERRAN_ENGINEERINGBAY);
+                TryBuildUnit(ABILITY_ID::RESEARCH_TERRANINFANTRYARMOR, UNIT_TYPEID::TERRAN_ENGINEERINGBAY);
+                TryBuildUnit(ABILITY_ID::RESEARCH_STIMPACK, UNIT_TYPEID::TERRAN_BARRACKSTECHLAB);
+                TryBuildUnit(ABILITY_ID::RESEARCH_COMBATSHIELD, UNIT_TYPEID::TERRAN_BARRACKSTECHLAB);
+                TryBuildUnit(ABILITY_ID::RESEARCH_CONCUSSIVESHELLS, UNIT_TYPEID::TERRAN_BARRACKSTECHLAB);
+                TryBuildUnit(ABILITY_ID::RESEARCH_PERSONALCLOAKING, UNIT_TYPEID::TERRAN_GHOSTACADEMY);
+TryBuildUnit(ABILITY_ID::RESEARCH_BANSHEECLOAKINGFIELD, UNIT_TYPEID::TERRAN_STARPORTTECHLAB);
+*/
+void researchTERRANINFANTRYWEAPONSLEVEL1(){
+    //for (const auto& upgrade : upgrades) {
+        if (CountUnitType(observation, UNIT_TYPEID::TERRAN_ARMORY) > 0) {
+            TryBuildUnit(ABILITY_ID::RESEARCH_TERRANINFANTRYWEAPONS, UNIT_TYPEID::TERRAN_ENGINEERINGBAY);
+        }
+    //}
+}
+void researchTERRANINFANTRYWEAPONSLEVEL2(){
+    //for (const auto& upgrade : upgrades) {
+        if (CountUnitType(observation, UNIT_TYPEID::TERRAN_ARMORY) > 0) {
+            if (upgrade == UPGRADE_ID::TERRANINFANTRYWEAPONSLEVEL1 && base_count > 2) {
+                TryBuildUnit(ABILITY_ID::RESEARCH_TERRANINFANTRYWEAPONS, UNIT_TYPEID::TERRAN_ENGINEERINGBAY);
+            }
+        }
+    //}
+}
+
+void researchTERRANINFANTRYWEAPONSLEVEL3(){
+    auto upgrades = observation->GetUpgrades();
+    //for (const auto& upgrade : upgrades) {
+        if (CountUnitType(observation, UNIT_TYPEID::TERRAN_ARMORY) > 0) {
+            if (upgrade == UPGRADE_ID::TERRANINFANTRYWEAPONSLEVEL2 && base_count > 2) {
+                TryBuildUnit(ABILITY_ID::RESEARCH_TERRANINFANTRYWEAPONS, UNIT_TYPEID::TERRAN_ENGINEERINGBAY);
+            }
+        }
+    //}
+}
+
+void researchCONCUSSIVESHELLS(){
+    auto upgrades = observation->GetUpgrades();
+    if (upgrades.empty()) {
+        TryBuildUnit(ABILITY_ID::RESEARCH_CONCUSSIVESHELLS, UNIT_TYPEID::TERRAN_BARRACKSTECHLAB);
+    }
+}
+void researchCOMBATSHIELD(){
+    auto upgrades = observation->GetUpgrades();
+    if (upgrades.empty()) {
+        TryBuildUnit(ABILITY_ID::RESEARCH_COMBATSHIELD, UNIT_TYPEID::TERRAN_BARRACKSTECHLAB);
+    }
+}
 void researchSTIMPACK(){
     auto upgrades = observation->GetUpgrades();
     if (upgrades.empty()) {
-            TryBuildUnit(ABILITY_ID::RESEARCH_STIMPACK, UNIT_TYPEID::TERRAN_BARRACKSTECHLAB);
+        TryBuildUnit(ABILITY_ID::RESEARCH_STIMPACK, UNIT_TYPEID::TERRAN_BARRACKSTECHLAB);
     }
 }
+
 void researchTERRANVEHICLEWEAPONSLEVEL1(){
     auto upgrades = observation->GetUpgrades();
     if (upgrades.empty()) {
@@ -147,6 +211,8 @@ void researchTERRANVEHICLEWEAPONSLEVEL3(){
     }
 }
 
+
+
 //ECONOMY RULE
 
 //COMBAT RULE -> RENAMED -> MILITARY RULE
@@ -174,6 +240,7 @@ void researchTERRANVEHICLEWEAPONSLEVEL3(){
     size_t banshee_count = CountUnitTypeTotal(observation, UNIT_TYPEID::TERRAN_MEDIVAC, UNIT_TYPEID::TERRAN_STARPORT, ABILITY_ID::TRAIN_BANSHEE);
 
 */
+/*
 void buildSTARPORTUNIT(){
     if (!starports.empty()) {
         for (const auto& starport : starports) {
@@ -229,8 +296,8 @@ void buildSTARPORTUNIT(){
         }
     }
 }
-
-void BUILDVIKING(){
+*/
+void buildVIKING(){
     for (const auto& starport : starports) {
         if (observation->GetUnit(starport->add_on_tag) == nullptr) {
             if (starport->orders.empty() && viking_count < 5) {
@@ -246,7 +313,7 @@ void BUILDVIKING(){
     }
 }
 
-void BUILDMEDIVAC(){
+void buildMEDIVAC(){
     for (const auto& starport : starports) {
         if (observation->GetUnit(starport->add_on_tag) == nullptr) {
             if (starport->orders.empty() && medivac_count < 5) {
@@ -262,3 +329,71 @@ void BUILDMEDIVAC(){
     }
 }
 
+
+/*
+        for (const auto& barrack : barracks) {
+            if (observation->GetUnit(barrack->add_on_tag) == nullptr) {
+                if (!mech_build_) {
+                    if (barrack->orders.empty() && marine_count < 20) {
+                        Actions()->UnitCommand(barrack, ABILITY_ID::TRAIN_MARINE);
+                    }
+                    else if (barrack->orders.empty() && observation->GetMinerals() > 1000 && observation->GetVespene() < 300) {
+                        Actions()->UnitCommand(barrack, ABILITY_ID::TRAIN_MARINE);
+                    }
+                }
+            }
+            else {
+                if (observation->GetUnit(barrack->add_on_tag)->unit_type == UNIT_TYPEID::TERRAN_BARRACKSREACTOR) {
+                    if (mech_build_) {
+                        if (barrack->orders.size() < 2 && marine_count < 5) {
+                            Actions()->UnitCommand(barrack, ABILITY_ID::TRAIN_MARINE);
+                        }
+                    }
+                    else{
+                        if (barrack->orders.size() < 2 && marine_count < 20) {
+                            Actions()->UnitCommand(barrack, ABILITY_ID::TRAIN_MARINE);
+                        }
+                        else if (observation->GetMinerals() > 1000 && observation->GetVespene() < 300) {
+                            Actions()->UnitCommand(barrack, ABILITY_ID::TRAIN_MARINE);
+                        }
+                    }
+                }
+                else {
+                    if (!mech_build_ && barrack->orders.empty()) {
+                        if (reaper_count < 2) {
+                            Actions()->UnitCommand(barrack, ABILITY_ID::TRAIN_REAPER);
+                        }
+                        else if (ghost_count < 4) {
+                            Actions()->UnitCommand(barrack, ABILITY_ID::TRAIN_GHOST);
+                        }
+                        else if (marauder_count < 10) {
+                            Actions()->UnitCommand(barrack, ABILITY_ID::TRAIN_MARAUDER);
+                        }
+                        else {
+                            Actions()->UnitCommand(barrack, ABILITY_ID::TRAIN_MARINE);
+                        }
+                    }
+                }
+            }
+        }
+*/
+
+void buildMARINE(){
+    for (const auto& barrack : barracks) {
+            if (observation->GetUnit(barrack->add_on_tag) == nullptr) {
+                    if (barrack->orders.empty() && marine_count < 20) {
+                        Actions()->UnitCommand(barrack, ABILITY_ID::TRAIN_MARINE);
+                    }
+            }
+    }
+}
+
+void buildMARAUDER(){
+    for (const auto& barrack : barracks) {
+            if (observation->GetUnit(barrack->add_on_tag) == nullptr) {
+                    if (barrack->orders.empty() && marauder_count < 20) {
+                        Actions()->UnitCommand(barrack, ABILITY_ID::TRAIN_MARAUDER);
+                    }
+            }
+    }
+}
