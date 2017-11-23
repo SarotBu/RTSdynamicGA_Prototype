7c17f63dd190bd7f5d072f313c8e857b19613de9
Sarot Busala <sabuza97@gmail.com>
2017-11-23 19:33:05 +0700

    Get everything together

---
+++ RTSdynamicGA_Prototype/DynamicScripting/RuleManager.cpp
@@ -0,0 +1,180 @@
+#include "RuleData.cpp"
+void ruleFunc1();
+void ruleFunc2();
+void ruleFunc3();
+void ruleFunc4();
+void ruleFunc5();
+void ruleFunc6();
+void ruleFunc7();
+void ruleFunc8();
+void ruleFunc9();
+void ruleFunc10();
+void ruleFunc11();
+void ruleFunc12();
+void ruleFunc13();
+void ruleFunc14();
+void ruleFunc15();
+void ruleFunc16();
+void ruleFunc17();
+void ruleFunc18();
+void ruleFunc19();
+void ruleFunc20();
+void ruleFunc21();
+void ruleFunc22();
+void ruleFunc23();
+void ruleFunc24();
+void ruleFunc25();
+void executeScriptViaID(int scriptID){
+	if (scriptID == 1){
+		ruleFunc1();
+	}else	if (scriptID == 2){
+		ruleFunc2();
+	}else	if (scriptID == 3){
+		ruleFunc3();
+	}else	if (scriptID == 4){
+		ruleFunc4();
+	}else	if (scriptID == 5){
+		ruleFunc5();
+	}else	if (scriptID == 6){
+		ruleFunc6();
+	}else	if (scriptID == 7){
+		ruleFunc7();
+	}else	if (scriptID == 8){
+		ruleFunc8();
+	}else	if (scriptID == 9){
+		ruleFunc9();
+	}else	if (scriptID == 10){
+		ruleFunc10();
+	}else	if (scriptID == 11){
+		ruleFunc11();
+	}else	if (scriptID == 12){
+		ruleFunc12();
+	}else	if (scriptID == 13){
+		ruleFunc13();
+	}else	if (scriptID == 14){
+		ruleFunc14();
+	}else	if (scriptID == 15){
+		ruleFunc15();
+	}else	if (scriptID == 16){
+		ruleFunc16();
+	}else	if (scriptID == 17){
+		ruleFunc17();
+	}else	if (scriptID == 18){
+		ruleFunc18();
+	}else	if (scriptID == 19){
+		ruleFunc19();
+	}else	if (scriptID == 20){
+		ruleFunc20();
+	}else	if (scriptID == 21){
+		ruleFunc21();
+	}else	if (scriptID == 22){
+		ruleFunc22();
+	}else	if (scriptID == 23){
+		ruleFunc23();
+	}else	if (scriptID == 24){
+		ruleFunc24();
+	}else	if (scriptID == 25){
+		ruleFunc25();
+	}
+}
+
+void ruleFunc1(){
+    buildORBITALCOMMAND();
+}
+
+void ruleFunc2(){
+    buildBARRACKADDON();
+}
+
+void ruleFunc3(){
+    buildFACTORY();
+}
+
+void ruleFunc4(){
+    buildSTARPORT();
+}
+
+void ruleFunc5(){
+    researchTERRANINFANTRYWEAPONSLEVEL1();
+}
+
+void ruleFunc6(){
+    researchTERRANINFANTRYWEAPONSLEVEL2();
+}
+
+void ruleFunc7(){
+    researchTERRANINFANTRYWEAPONSLEVEL3();
+}
+
+void ruleFunc8(){
+    researchCONCUSSIVESHELLS();
+}
+
+void ruleFunc9(){
+    researchCOMBATSHIELD();
+}
+
+void ruleFunc10(){
+    researchSTIMPACK();
+}
+
+void ruleFunc11(){
+    researchTERRANVEHICLEWEAPONSLEVEL1();
+}
+
+void ruleFunc12(){
+    researchTERRANVEHICLEWEAPONSLEVEL2();
+}
+
+void ruleFunc13(){
+    researchTERRANVEHICLEWEAPONSLEVEL3();
+}
+
+void ruleFunc14(){
+    buildVIKING(5);
+}
+
+void ruleFunc15(){
+    buildMEDIVAC(5);
+}
+
+void ruleFunc16(){
+    buildMARINE(5);
+}
+
+void ruleFunc17(){
+    buildMARAUDER(5);
+}
+
+void ruleFunc18(){
+    buildMARINE(6);
+}
+
+void ruleFunc19(){
+    buildMARINE(7);
+}
+
+void ruleFunc20(){
+    buildMARINE(8);
+}
+
+void ruleFunc21(){
+    buildMARINE(9);
+}
+
+void ruleFunc22(){
+    buildMARINE(10);
+}
+
+void ruleFunc23(){
+    buildMEDIVAC(2);
+}
+
+void ruleFunc24(){
+    buildMEDIVAC(3);
+}
+
+void ruleFunc25(){
+    buildMEDIVAC(4);
+}
+
---
+++ RTSdynamicGA_Prototype/sabulib.h
@@ -0,0 +1,9 @@
+#include "./common/bot_examples.h"
+#include "./common/bot_examples.cc"
+#include "./common/Ruleset.h"
+
+#include "./DynamicScripting/DynamicScripting.cpp"
+#include "./DynamicScripting/RuleData.cpp"
+#include "./DynamicScripting/RuleManager.cpp"
+
+
